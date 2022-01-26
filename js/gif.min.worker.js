b4ce52307fb302cdedf9808111bc4da38d06c3e7
Johnathon Selstad <makeshifted@gmail.com>
2022-01-26 11:54:45 -0800

    One last fix...

--- js/gif.min.worker.js
+++ js/gif.min.worker.js
@@ -1,3 +1,3 @@
-importScripts('/js/gif.min.js');
+importScripts('/gifcorder/js/gif.min.js');
 onmessage = (e) => { postMessage(gifmin(e.data.buffer, e.data.colors)); }
 onerror   = (e) => { console.error(e.message); }
